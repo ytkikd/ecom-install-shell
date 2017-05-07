c54476cbcebc118d0f9d2c6b06294bd4e7e4cc13
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-05-07 13:57:07 +0900

    ecomマップのインストールで、ファイル削除などするシェルスクリプトを作成

---
+++ 3_6.sh
@@ -0,0 +1,11 @@
+tar zxvf OpenLayers-2.13.1.tar.gz
+mv OpenLayers-2.13.1 webapps/map/OpenLayers
+#
+tar zxvf dojo-release-1.9.4.tar.gz
+mkdir webapps/map/js/
+mv dojo-release-1.9.4 webapps/map/js/dojo-1.9
+#
+unzip IPAfont00303.zip
+mkdir webapps/map/WEB-INF
+mv IPAfont00303 webapps/map/WEB-INF/fonts
+
