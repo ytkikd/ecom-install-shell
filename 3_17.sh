2cdbd596122ec925fd109e7488ff81db35b848e1
Yutaka IKEDA <y_ikeda@mub.biglobe.ne.jp>
2017-05-18 15:33:10 +0900

    3.17  3.18章のファイル削除追加

---
+++ 3_17.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd /home/map
+echo "rm -rf webapps/map/WEB-INF/classes/jp"
+rm -rf webapps/map/WEB-INF/classes/jp
+echo "rm -rf webapps/map/WEB-INF/lib"
+rm -rf webapps/map/WEB-INF/lib
---
+++ 3_18.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+cd /home/map
+rm -f geoserver/data_dir/layergroups/*
+rm -rf geoserver/data_dir/workspaces/cite
+rm -rf geoserver/data_dir/workspaces/it.geosolutions
+rm -rf geoserver/data_dir/workspaces/nurc
+rm -rf geoserver/data_dir/workspaces/sde
+rm -rf geoserver/data_dir/workspaces/sf
+rm -rf geoserver/data_dir/workspaces/tiger
+rm -rf geoserver/data_dir/workspaces/topp
